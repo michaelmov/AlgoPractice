dc9c9a9d1eb0a14248b1eb82a5964f72880f46c9
Michael Movsesov <michael.movsesov@gmail.com>
2021-09-06 06:56:14 -0700

    Added problems + directions

---
+++ non-constructible-change/non-constructible-change.test.ts
@@ -0,0 +1,27 @@
+import { nonConstructibleChange } from "./non-constructible-change";
+
+
+
+test("nonConstructibleChange function is defined", () => {
+  expect(nonConstructibleChange).toBeDefined();
+});
+
+
+test('nonConstructibleChange function returns a correct value', () => {
+  const input = [1, 1, 4]
+
+  expect(nonConstructibleChange(input)).toEqual(3)
+})
+
+test('nonConstructibleChange function returns a correct value', () => {
+  const input = [1, 1, 3]
+
+  expect(nonConstructibleChange(input)).toEqual(6)
+})
+
+
+test('nonConstructibleChange function returns a correct value', () => {
+  const input = [5, 7, 1, 1, 2, 3, 22]
+
+  expect(nonConstructibleChange(input)).toEqual(20)
+})
