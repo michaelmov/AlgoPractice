b3858cff8a13dd2e13d15779f175e038c248e771
Michael Movsesov <michael.movsesov@gmail.com>
2021-09-12 14:37:20 -0700

    Add nth-fibonacci

--- nth-fibonacci/nth-fibonacci.test.ts
+++ nth-fibonacci/nth-fibonacci.test.ts
@@ -1,14 +1,14 @@
 import { getNthFib, getNthFib2 } from './nth-fibonacci';
 
-// test('getNthFib should be defined', () => {
-//   expect(getNthFib).toBeDefined();
-//   expect(getNthFib2).toBeDefined();
-// });
+test('getNthFib should be defined', () => {
+  expect(getNthFib).toBeDefined();
+  expect(getNthFib2).toBeDefined();
+});
 
-// test('Should return correct result for 6', () => {
-//   expect(getNthFib(6)).toEqual(5);
-//   expect(getNthFib2(5)).toEqual(5);
-// });
+test('Should return correct result for 6', () => {
+  expect(getNthFib(6)).toEqual(5);
+  expect(getNthFib2(5)).toEqual(5);
+});
 
 test('Should return correct result for 100', () => {
   expect(getNthFib(18)).toEqual(1597);
